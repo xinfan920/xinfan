6324b20e9e5080f5d1d5660666942b16ab4122a6
xinfan920 <2236409986@qq.com>
2026-07-19 00:10:38 +0800

    Summary

--- genManifest.js
+++ genManifest.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 const EXT_DIR = './';          // 脚本放在扩展根目录运行
 const OUTPUT = 'manifest.json';
 const VERSION = '0.70.5';      // 版本号，每次更新请修改
-const UPDATE_NOTE = '0.70.5：部分角色ai优化，音乐播放方式修改'; // 更新说明
+const UPDATE_NOTE = '0.70.5：平将门削弱'; // 更新说明
 
 // 递归获取所有文件相对路径
 function walkDir(dir, baseDir, fileList = []) {
