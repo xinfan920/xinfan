438310e0679c9b34e4d6a0d34791b1537cba7a44
xinfan920 <2236409986@qq.com>
2026-09-21 23:38:35 +0800

    Summary

--- daoru/content.js
+++ daoru/content.js
@@ -3,29 +3,5 @@ import update from '../daoru/update.js';
 //扩展主文件
 //联机扩展一般不用设置
 export async function content(config, pack) {
-			//【更新说明】
-			let xinfan_gengxin = [
-				{
-					type: "text",
-					data: `○新增武将：`,
-				},
-				{
-					type: "players", data: [
-						"xinfan_hairen",
 
-					]
-				},
-				{
-					type: "text",
-					data: `○武将调整：`,
-				},
-				{
-					type: "players", data: [
-						"xinfan_pingjiangmen",
-						"xinfan_huangkulou"
-					]
-				},
-			];
-			game.showExtensionChangeLog(xinfan_gengxin, "阴阳师杀");
-			if (lib.config.extension_阴阳师杀_auto_update && navigator.onLine) update(false);
 };
